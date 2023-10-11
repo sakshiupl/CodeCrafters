7578289c7ace8b0220cc4ad2bbdf7008b2e4457f
SaUPL <96293439+sakshiupl@users.noreply.github.com>
2023-10-11 11:18:27 -0400

    changes for userDetail component

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {
-  Typography
+  TextField,
+  Button
 } from '@mui/material';
 import './userDetail.css';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
 
 
 /**
@@ -11,17 +15,74 @@ import './userDetail.css';
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userId: this.props.match.params.userId,
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.match.params.userId !== prevProps.match.params.userId) {
+      this.setState({ userId: this.props.match.params.userId })
+    }
   }
 
   render() {
+    const {userId} = this.state;
+    var userDetails = window.models.userModel(userId);
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+      <div>
+        <Button
+          variant="contained"
+          size="medium"
+          component={Link}
+          to={`/photos/${userId}`}
+          className="button"
+        >
+            USER PHOTOS
+        </Button>
+        <TextField
+          disabled
+          fullWidth
+          id="outlined-disabled"
+          label="First Name"
+          className="custom-field"
+          value={userDetails.first_name}
+        />
+        <TextField
+          disabled
+          fullWidth
+          id="outlined-disabled"
+          label="Last Name"
+          className="custom-field"
+          value={userDetails.last_name}
+        />
+        <TextField
+          disabled
+          fullWidth
+          id="outlined-disabled"
+          label="Location"
+          className="custom-field"
+          value={userDetails.location}
+        />
+        <TextField
+          disabled
+          fullWidth
+          id="outlined-disabled"
+          label="Description"
+          multiline
+          rows={5}
+          className="custom-field"
+          value={userDetails.description}
+        />
+        <TextField
+          disabled
+          fullWidth
+          id="outlined-disabled"
+          label="Occupation"
+          className="custom-field"
+          value={userDetails.occupation}
+        />
+    </div>
     );
   }
 }
