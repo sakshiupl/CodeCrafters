16cbccc1fdd79808289c0c41a9319cd8a0e677f0
Sravani1915 <64299904+Sravani1915@users.noreply.github.com>
2023-10-13 11:59:19 -0400

    Update TopBar.jsx

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -3,6 +3,7 @@ import {
   AppBar, Toolbar, Typography
 } from '@mui/material';
 import './TopBar.css';
+import fetchModel from "../../lib/fetchModelData";
 
 /**
  * Define TopBar, a React componment of project #5
@@ -10,17 +11,45 @@ import './TopBar.css';
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      app_version: undefined
+  };
   }
 
+  componentDidMount() {
+    this.handleAppVersionChange();
+}
+
+ handleAppVersionChange(){
+    const app_version = this.state.app_version;
+    if (app_version === undefined){
+        fetchModel("/test/info")
+            .then((response) =>
+            {
+                this.setState({
+                    app_version: response.data
+                });
+            });
+    }
+}
+
   render() {
-    return (
+   const {app_version} = this.state;
+    return app_version ? (
       <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar>
+        <Toolbar className='topbar'>
           <Typography variant="h5" color="inherit">
-              This is the TopBar component
+              This is Code Crafter's TopBar component
           </Typography>
+          <Typography variant="h5" color="inherit">
+              {this.props.page_content}
+          </Typography>
+          <Typography variant="h5" component="div" color="inherit">Version: {this.state.app_version.__v}</Typography> 
+  
         </Toolbar>
       </AppBar>
+    ) : (
+      <div />
     );
   }
 }
