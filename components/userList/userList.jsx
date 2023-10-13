087b0effbf5d387f898ec09c4a0b499bb636046f
Sravani1915 <64299904+Sravani1915@users.noreply.github.com>
2023-10-13 11:57:58 -0400

    Update userList.jsx

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -4,11 +4,11 @@ import {
   List,
   ListItem,
   ListItemText,
-  Typography,
 }
 from '@mui/material';
 import { Link } from 'react-router-dom';
 import './userList.css';
+import fetchModel from "../../lib/fetchModelData";
 
 /**
  * Define UserList, a React component of project #5
@@ -17,13 +17,45 @@ class UserList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userListModel: window.models.userListModel(),
-    }
+      userListModel: undefined,
+      user_id: undefined
+    };
   }
+
+  
+    componentDidMount() {
+        this.handleUserListChange();
+    }
+
+    componentDidUpdate() {
+      const new_user_id = this.props.match?.params.userId;
+      const current_user_id = this.state.user_id;
+      if (current_user_id  !== new_user_id){
+          this.handleUserChange(new_user_id);
+      }
+  }
+
+  handleUserChange(user_id){
+    this.setState({
+        user_id: user_id
+    });
+  }
+
+
+    handleUserListChange(){
+      fetchModel("/user/list")
+          .then((response) =>
+          {
+              this.setState({
+                userListModel: response.data
+              });
+          });
+    }
 
   render() {
     const { userListModel } = this.state;
-    return (
+   return userListModel ? 
+     (
       <div>
         {userListModel.map((user) => (
           <Link to={`/users/${user._id}`} key={user._id} className="userlink">
@@ -36,7 +68,7 @@ class UserList extends React.Component {
           </Link>
         ))}
       </div>
-    );
+    ) : ( <div />);
   }
 }
 
